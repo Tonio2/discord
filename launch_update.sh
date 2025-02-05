be480fd712013b131dd3fc3fb82f9c5cfe275101
Antoine Labalette <labalette.antoine@gmail.com>
2025-02-06 00:42:29 +0100

    Start discord after executing update check

--- launch_update.sh
+++ launch_update.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gnome-terminal -- bash -c "$HOME/scripts/discord/update.sh; exit_code=\$?; if [[ \$exit_code -ne 0 ]]; then echo 'An error occurred. Check logs for details.'; read -p 'Press Enter to close...'; fi; exit \$exit_code"
+
+"$HOME/scripts/discord/update.sh"
+exit_code=$?
+
+if [[ $exit_code -ne 0 ]]; then
+    zenity --error --text="Discord update failed.\n\nCheck logs for details at $HOME/scripts/discord/logs.txt"
+    exit $exit_code
+else
+    systemd-run --user --quiet --description="Discord Detached" discord
+    exit 0
+fi
